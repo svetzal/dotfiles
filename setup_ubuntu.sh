c02152d8d51c9b3a1bdcfb9d358b2f1fad2f48e7
Stacey Vetzal <stacey@vetzal.com>
2022-02-18 21:19:00 -0500

    adding ubuntu setup script

---
+++ setup_ubuntu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+
+sudo apt install gnupg2 curl autoconf automake bison libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool libyaml-dev sqlite3 libgmp-dev libreadline-dev libssl-dev
+
+# Starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# RVM
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+
